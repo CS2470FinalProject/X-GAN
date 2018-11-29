9e419d88f58493f85f6d096450bd684e96c3c089
Yang Yang <675803882@qq.com>
2018-11-29 11:44:02 -0500

    Some modifications

--- module.py
+++ module.py
@@ -11,14 +11,14 @@ def discriminator(image, options, domain_name):
         # image is (64 x 64 x input_c_dim)
         h1 = lrelu(conv2d(image, options.dcf_dim, name = 'h1_conv'))
         # h1 is (32 x 32 x args.ndcf)
-        h2 = lrelu(instance_norm(conv2d(h1, options.dcf_dim * 2, name = 'h2_conv_' + domain_name), 'h_bn2_' + domain_name))
+        h2 = lrelu(instance_norm(conv2d(h1, options.dcf_dim * 2, name = 'h2_conv'), 'h_bn2'))
         # h2 is (16 x 16 x args.ndcf * 2)
-        h3 = lrelu(instance_norm(conv2d(h2, options.dcf_dim * 2, name='h3_conv_' + domain_name), 'h_bn3_' + domain_name))
+        h3 = lrelu(instance_norm(conv2d(h2, options.dcf_dim * 2, name='h3_conv'), 'h_bn3'))
         # h3 is (8 x 8 x args.ndcf * 2)
-        h4 = lrelu(instance_norm(conv2d(h3, options.dcf_dim * 2, name='h4_conv_' + domain_name), 'h_bn4_' + domain_name))
+        h4 = lrelu(instance_norm(conv2d(h3, options.dcf_dim * 2, name='h4_conv'), 'h_bn4'))
         # h4 is (4 x 4 x args.ndcf * 2)
         h5 = h4.reshape(h4, [1, 1, -1])
-        h5 = linear(h5, 1, scope = domain_name)
+        h5 = linear(h5, 1)
         # h5 is (1 x 1 x 1)
 
     return h5
@@ -27,9 +27,9 @@ def encoder(image, options, domain_name):
     
     with tf.variable_scope("encoder_" + domain_name, reuse=tf.AUTO_REUSE):
         # image is (64 x 64 x input_c_dim)
-        e1 = instance_norm(conv2d(image, options.ef_dim, name = 'e1_conv_' + domain_name), 'e_bn1_' + domain_name)
+        e1 = instance_norm(conv2d(image, options.ef_dim, name = 'e1_conv'), 'e_bn1')
         # e1 is (32 x 32 x args.nef)
-        e2 = instance_norm(conv2d(lrelu(e1), options.ef_dim * 2, name = 'e2_conv' + domain_name), 'e_bn2_' + domain_name)
+        e2 = instance_norm(conv2d(lrelu(e1), options.ef_dim * 2, name = 'e2_conv'), 'e_bn2')
         # e2 is (16 x 16 x args.nef * 2)
 
     with tf.variable_scope("encoder_sharing", reuse=tf.AUTO_REUSE):
@@ -57,11 +57,11 @@ def decoder(image, options, domain_name):
 
     
     with tf.variable_scope("decoder_" + domain_name, reuse=tf.AUTO_REUSE):
-        d3 = instance_norm(deconv2d(d2, options.df_dim * 2, name='d3_dconv_' + domain_name), 'd_bn3_' + domain_name)
+        d3 = instance_norm(deconv2d(d2, options.df_dim * 2, name='d3_dconv'), 'd_bn3')
         # d3 is (16 x 16 x args.ndf * 2)
-        d4 = instance_norm(deconv2d(d3, options.df_dim, name='d4_dconv_' + domain_name), 'd_bn4_' + domain_name)
+        d4 = instance_norm(deconv2d(d3, options.df_dim, name='d4_dconv'), 'd_bn4')
         # d4 is (32 x 32 x args.ndf)
-        d5 = instance_norm(deconv2d(d4, options.output_c_dim, name='d5_dconv_' + domain_name), 'd_bn5' + domain_name)
+        d5 = instance_norm(deconv2d(d4, options.output_c_dim, name='d5_dconv'), 'd_bn5')
         # d5 is (64 x 64 x args.output_nc)
 
     return d5
